ae5756ac3fc6599d40fcf04c6020a152e5162315
Amy Lee <amyleeannebennett@gmail.com>
2016-02-16 12:03:32 -0600

    changed to angular

---
+++ client/controllers/QuoteCtrl.js
@@ -0,0 +1,8 @@
+"use strict";
+var myApp = angular.module("myApp");
+
+myApp.controller('QuoteCtrl', ['$scope', '$http','$location','$routeParams', function($scope, $http,$location, $routeParams){
+  console.log('Customer Controller Initialized...');
+
+//dependencies
+}]);
---
+++ routes/getQuote.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+// const ctrl = require('../client/controllers/getQuoteCtrl');
+
+// router.get('/quotes', (req, res) =>
+// {
+//   res.render("quotes")
+// })
+
+// router.get('/quotes', ctrl.index);
+
+module.exports = router;
